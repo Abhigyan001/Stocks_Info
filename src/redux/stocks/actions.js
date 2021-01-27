28f1ffac39fa3a772e1d09f9c278af2d46eb96d6
Abhiyan001 <abhigyanmahanta001@gmail.com>
2021-01-27 19:42:20 +0530

    Set up redux store with API urls

---
+++ src/redux/stocks/actions.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { FETCH_STOCK_REQUEST, FETCH_STOCK_SUCCESS, FETCH_STOCK_FAILURE } from './types';
+
+export const fetchStocksRequest = () => ({
+  type: FETCH_STOCK_REQUEST,
+});
+
+export const fetchStocksSuccess = stocks => ({
+  type: FETCH_STOCK_SUCCESS,
+  payload: stocks,
+});
+
+export const fetchStocksFailure = error => ({
+  type: FETCH_STOCK_FAILURE,
+  payload: error,
+});
+
+export const fetchStocks = () => function (dispatch) {
+  dispatch(fetchStocksRequest());
+  axios
+    .get(API_URL);
+};
