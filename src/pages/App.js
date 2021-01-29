58e0abcb8e53aa12aa4d4119960ede0e45818f19
Abhiyan001 <abhigyanmahanta001@gmail.com>
2021-01-29 17:52:49 +0530

    Add default not found page

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,12 +1,18 @@
-  
+import { useDispatch } from 'react-redux';
 import { Container } from 'react-bootstrap';
 import TopNav from '../components/Nav';
 import Stockcontainer from '../container/StockContainer';
+import { fetchStocks } from '../redux/index';
 
 function App() {
+  const dispatch = useDispatch();
+  const handleOnSelect = e => {
+    dispatch(fetchStocks(e.target.value));
+  };
+
   return (
     <Container className="pt-5 pb-5">
-      <TopNav />
+      <TopNav handleOnSelect={handleOnSelect} />
       <Stockcontainer />
     </Container>
   );
--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -4,10 +4,15 @@ import Footer from '../components/Footer';
 import TopNavTicker from '../components/TopNavTicker';
 import StockDetailsContainer from '../container/StockDetailsContainer';
 
-export default function Detailpage() {
+export default function DetailPage() {
+  const handleFormClick = e => {
+    e.preventDefault();
+    const url = `/stock/${e.target[0].value}`;
+    window.location.href = url;
+  };
   return (
     <Container className="pt-5 pb-5">
-      <TopNavTicker />
+      <TopNavTicker handleFormClick={handleFormClick} />
       <StockDetailsContainer />
       <Footer />
     </Container>
