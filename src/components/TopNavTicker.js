b0dcaab5910cd76831854f8041d483aed430e133
Abhiyan001 <abhigyanmahanta001@gmail.com>
2021-01-29 13:17:34 +0530

    Update nabigation bar with market news

--- src/components/TopNavTicker.js
+++ src/components/TopNavTicker.js
@@ -28,4 +28,33 @@ export default function TopNavTicker({ handleFormClick }) {
       </Form>
     </Navbar>
   );
+}import React from 'react';
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+} from 'react-bootstrap';
+
+// eslint-disable-next-line react/prop-types
+export default function TopNavTicker({ handleFormClick }) {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="/">STOCKIST</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/news">Stock News</Nav.Link>
+      </Nav>
+      <Form inline onSubmit={handleFormClick}>
+        <FormControl
+          type="text"
+          placeholder="Search Stock"
+          className="mr-sm-2"
+          name="ticker"
+        />
+        <Button type="submit" variant="outline-success">Search</Button>
+      </Form>
+    </Navbar>
+  );
 }
