66c16a3b6b70c48e52423d1dbe7f3fbb97a4a8fe
Abhiyan001 <abhigyanmahanta001@gmail.com>
2021-01-29 23:11:31 +0530

    Design footer nav

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="container">
-        <span className="text-muted">
+      <div className="container bg-dark p-3 text-center mt-4">
+        <span className="text-light">
           &copy;
           { ` ${(new Date()).getFullYear()} All rights reserved | site by Abhigyan Mahanta.`}
         </span>
